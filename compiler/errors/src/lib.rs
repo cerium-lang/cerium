3a8aea9f127edc44fa0c6ee3385f37bd137a47c6
Yhya <yhyamobile57@gmail.com>
2023-12-24 08:10:12 +0200

    change error formatting

--- compiler/errors/src/lib.rs
+++ compiler/errors/src/lib.rs
@@ -32,7 +32,7 @@ impl<'a> Display for Error<'a> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
             f,
-            "{} at {}:{}: {}",
+            "{}:{}:{}: {}",
             self.file_path.display(),
             self.position.line,
             self.position.column,
