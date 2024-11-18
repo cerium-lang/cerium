db1993c3452302f1eb2d9c39a9043b337af2f114
Yhya Ibrahim <yhyamobile57@gmail.com>
2024-11-18 09:08:36 +0200

    feat(Cli): implement a `run` command

--- src/main.zig
+++ src/main.zig
@@ -23,6 +23,7 @@ pub const Cli = struct {
 
     const Command = union(enum) {
         compile: Compile,
+        run: Run,
         help,
 
         const Compile = struct {
@@ -34,7 +35,7 @@ pub const Cli = struct {
 
             const usage =
                 \\Usage:
-                \\  {s} compile <input-file-path>
+                \\  {s} compile <input-file-path> [options]
                 \\
                 \\Options:
                 \\  --output <output-file-path>    -- specify the output file path
@@ -47,14 +48,34 @@ pub const Cli = struct {
                 \\
             ;
         };
+
+        const Run = struct {
+            input_file_path: []const u8,
+            runner_kind: RunnerKind,
+            target: std.Target,
+            arguments: []const []const u8,
+
+            const usage =
+                \\Usage:
+                \\  {s} run <input-file-path> [options] [-- [arguments]]
+                \\
+                \\Options:
+                \\  --runner <runner-kind>         -- specify the runner kind
+                \\                                    [executable (default), none]
+                \\  --target <arch-os-abi>         -- specify the target query
+                \\
+                \\
+            ;
+        };
     };
 
     const usage =
         \\Usage:
-        \\  {s} <command> [arguments]
+        \\  {s} <command> [options]
         \\
         \\Commands:
-        \\  compile <input-file-path>     -- compile certain file
+        \\  compile                       -- compile certain file
+        \\  run                           -- compile certain file into an executable and run it
         \\  help                          -- print this help message
         \\
         \\
@@ -93,6 +114,52 @@ pub const Cli = struct {
         };
     }
 
+    fn parseRunnerKindOption(self: Cli, raw_runner_kind: []const u8) ?RunnerKind {
+        if (std.mem.eql(u8, raw_runner_kind, "executable")) {
+            return .executable;
+        } else if (std.mem.eql(u8, raw_runner_kind, "none")) {
+            return .none;
+        } else {
+            std.debug.print(Command.Compile.usage, .{self.program});
+
+            std.debug.print("Error: unrecognized runner kind: {s}\n", .{raw_runner_kind});
+
+            return null;
+        }
+    }
+
+    fn parseOutputKindOption(self: Cli, raw_output_kind: []const u8) ?OutputKind {
+        if (std.mem.eql(u8, raw_output_kind, "assembly")) {
+            return .assembly;
+        } else if (std.mem.eql(u8, raw_output_kind, "object")) {
+            return .object;
+        } else if (std.mem.eql(u8, raw_output_kind, "executable")) {
+            return .executable;
+        } else if (std.mem.eql(u8, raw_output_kind, "none")) {
+            return .none;
+        } else {
+            std.debug.print(Command.Compile.usage, .{self.program});
+
+            std.debug.print("Error: unrecognized output kind: {s}\n", .{raw_output_kind});
+
+            return null;
+        }
+    }
+
+    fn parseTargetQueryOption(raw_target_query: []const u8) ?std.Target {
+        const target_query = std.Target.Query.parse(.{ .arch_os_abi = raw_target_query }) catch |err| {
+            std.debug.print("Error: could not parse target query: {s}\n", .{errorDescription(err)});
+
+            return null;
+        };
+
+        return std.zig.system.resolveTargetQuery(target_query) catch |err| {
+            std.debug.print("Error: could not resolve target query: {s}\n", .{errorDescription(err)});
+
+            return null;
+        };
+    }
+
     fn parse(allocator: std.mem.Allocator, argument_iterator: *std.process.ArgIterator) ?Cli {
         var self: Cli = .{
             .allocator = allocator,
@@ -125,21 +192,7 @@ pub const Cli = struct {
                         };
                     } else if (std.mem.eql(u8, next_argument, "--emit")) {
                         if (argument_iterator.next()) |raw_output_kind| {
-                            if (std.mem.eql(u8, raw_output_kind, "assembly")) {
-                                output_kind = .assembly;
-                            } else if (std.mem.eql(u8, raw_output_kind, "object")) {
-                                output_kind = .object;
-                            } else if (std.mem.eql(u8, raw_output_kind, "executable")) {
-                                output_kind = .executable;
-                            } else if (std.mem.eql(u8, raw_output_kind, "none")) {
-                                output_kind = .none;
-                            } else {
-                                std.debug.print(Command.Compile.usage, .{self.program});
-
-                                std.debug.print("Error: unrecognized output kind: {s}\n", .{raw_output_kind});
-
-                                return null;
-                            }
+                            output_kind = self.parseOutputKindOption(raw_output_kind) orelse return null;
                         } else {
                             std.debug.print(Command.Compile.usage, .{self.program});
 
@@ -149,17 +202,7 @@ pub const Cli = struct {
                         }
                     } else if (std.mem.eql(u8, next_argument, "--runner")) {
                         if (argument_iterator.next()) |raw_runner_kind| {
-                            if (std.mem.eql(u8, raw_runner_kind, "executable")) {
-                                runner_kind = .executable;
-                            } else if (std.mem.eql(u8, raw_runner_kind, "none")) {
-                                runner_kind = .none;
-                            } else {
-                                std.debug.print(Command.Compile.usage, .{self.program});
-
-                                std.debug.print("Error: unrecognized runner kind: {s}\n", .{raw_runner_kind});
-
-                                return null;
-                            }
+                            runner_kind = self.parseRunnerKindOption(raw_runner_kind) orelse return null;
                         } else {
                             std.debug.print(Command.Compile.usage, .{self.program});
 
@@ -169,17 +212,7 @@ pub const Cli = struct {
                         }
                     } else if (std.mem.eql(u8, next_argument, "--target")) {
                         if (argument_iterator.next()) |raw_target_query| {
-                            const target_query = std.Target.Query.parse(.{ .arch_os_abi = raw_target_query }) catch |err| {
-                                std.debug.print("Error: could not parse target query: {s}\n", .{errorDescription(err)});
-
-                                return null;
-                            };
-
-                            target = std.zig.system.resolveTargetQuery(target_query) catch |err| {
-                                std.debug.print("Error: could not resolve target query: {s}\n", .{errorDescription(err)});
-
-                                return null;
-                            };
+                            target = parseTargetQueryOption(raw_target_query) orelse return null;
                         } else {
                             std.debug.print(Command.Compile.usage, .{self.program});
 
@@ -205,6 +238,75 @@ pub const Cli = struct {
                         .target = target,
                     },
                 };
+            } else if (std.mem.eql(u8, argument, "run")) {
+                const input_file_path = argument_iterator.next() orelse {
+                    std.debug.print(Command.Run.usage, .{self.program});
+
+                    std.debug.print("Error: expected input file path\n", .{});
+
+                    return null;
+                };
+
+                var runner_kind: RunnerKind = .executable;
+                var target: std.Target = builtin.target;
+
+                while (argument_iterator.next()) |next_argument| {
+                    if (std.mem.eql(u8, next_argument, "--runner")) {
+                        if (argument_iterator.next()) |raw_runner_kind| {
+                            runner_kind = self.parseRunnerKindOption(raw_runner_kind) orelse return null;
+                        } else {
+                            std.debug.print(Command.Run.usage, .{self.program});
+
+                            std.debug.print("Error: expected runner kind\n", .{});
+
+                            return null;
+                        }
+                    } else if (std.mem.eql(u8, next_argument, "--target")) {
+                        if (argument_iterator.next()) |raw_target_query| {
+                            target = parseTargetQueryOption(raw_target_query) orelse return null;
+                        } else {
+                            std.debug.print(Command.Run.usage, .{self.program});
+
+                            std.debug.print("Error: expected target query\n", .{});
+
+                            return null;
+                        }
+                    } else if (std.mem.eql(u8, next_argument, "--")) {
+                        var remaining_arguments: std.ArrayListUnmanaged([]const u8) = .{};
+
+                        while (argument_iterator.next()) |remaining_argument| {
+                            remaining_arguments.append(self.allocator, remaining_argument) catch |err| {
+                                std.debug.print("Error: {s}\n", .{errorDescription(err)});
+
+                                return null;
+                            };
+                        }
+
+                        self.command = .{
+                            .run = .{
+                                .input_file_path = input_file_path,
+                                .runner_kind = runner_kind,
+                                .target = target,
+                                .arguments = remaining_arguments.toOwnedSlice(self.allocator) catch |err| {
+                                    std.debug.print("Error: {s}\n", .{errorDescription(err)});
+
+                                    return null;
+                                },
+                            },
+                        };
+
+                        return self;
+                    }
+                }
+
+                self.command = .{
+                    .run = .{
+                        .input_file_path = input_file_path,
+                        .runner_kind = runner_kind,
+                        .target = target,
+                        .arguments = &.{},
+                    },
+                };
             } else if (std.mem.eql(u8, argument, "help")) {
                 self.command = .help;
             } else {
@@ -227,9 +329,14 @@ pub const Cli = struct {
         return self;
     }
 
-    fn executeCompileCommand(self: Cli) u8 {
-        const options = self.command.?.compile;
-
+    fn compileStep(
+        self: Cli,
+        input_file_path: []const u8,
+        maybe_output_file_path: ?[]const u8,
+        output_kind: OutputKind,
+        runner_kind: RunnerKind,
+        target: std.Target,
+    ) u8 {
         const cerium_lib_dir = Compilation.Environment.openCeriumLibrary() catch {
             std.debug.print("Error: could not open the cerium library directory\n", .{});
 
@@ -238,12 +345,12 @@ pub const Cli = struct {
 
         var compilation = Compilation.init(self.allocator, .{
             .cerium_lib_dir = cerium_lib_dir,
-            .target = options.target,
+            .target = target,
         });
 
         defer compilation.deinit();
 
-        if (options.runner_kind == .executable) {
+        if (runner_kind == .executable) {
             var runner_file_path_buf: [std.fs.max_path_bytes]u8 = undefined;
 
             const runner_file_path = cerium_lib_dir.realpath(
@@ -262,7 +369,7 @@ pub const Cli = struct {
             };
         }
 
-        compilation.put(options.input_file_path) catch |err| {
+        compilation.put(input_file_path) catch |err| {
             std.debug.print("Error: {s}\n", .{errorDescription(err)});
 
             return 1;
@@ -276,10 +383,10 @@ pub const Cli = struct {
 
         if (compilation.pipeline.failed) return 1;
 
-        if (options.output_kind == .assembly) {
+        if (output_kind == .assembly) {
             const assembly_file_path = std.fmt.allocPrintZ(self.allocator, "{s}{s}", .{
-                options.maybe_output_file_path orelse std.fs.path.stem(options.input_file_path),
-                if (options.maybe_output_file_path != null) "" else ".s",
+                maybe_output_file_path orelse std.fs.path.stem(input_file_path),
+                if (maybe_output_file_path != null) "" else ".s",
             }) catch |err| {
                 std.debug.print("Error: {s}\n", .{errorDescription(err)});
 
@@ -288,15 +395,15 @@ pub const Cli = struct {
 
             defer self.allocator.free(assembly_file_path);
 
-            compilation.emit(assembly_file_path, options.output_kind) catch |err| {
+            compilation.emit(assembly_file_path, output_kind) catch |err| {
                 std.debug.print("Error: could not emit assembly file: {s}\n", .{errorDescription(err)});
 
                 return 1;
             };
-        } else if (options.output_kind == .object or options.output_kind == .executable) {
+        } else if (output_kind == .object or output_kind == .executable) {
             const object_file_path = std.fmt.allocPrintZ(self.allocator, "{s}{s}", .{
-                options.maybe_output_file_path orelse std.fs.path.stem(options.input_file_path),
-                if (options.maybe_output_file_path != null) "" else options.target.ofmt.fileExt(options.target.cpu.arch),
+                maybe_output_file_path orelse std.fs.path.stem(input_file_path),
+                if (maybe_output_file_path != null and output_kind == .object) "" else target.ofmt.fileExt(target.cpu.arch),
             }) catch |err| {
                 std.debug.print("Error: {s}\n", .{errorDescription(err)});
 
@@ -305,14 +412,17 @@ pub const Cli = struct {
 
             defer self.allocator.free(object_file_path);
 
-            compilation.emit(object_file_path, options.output_kind) catch |err| {
+            compilation.emit(object_file_path, output_kind) catch |err| {
                 std.debug.print("Error: could not emit object file: {s}\n", .{errorDescription(err)});
 
                 return 1;
             };
 
-            if (options.output_kind == .executable) {
-                const linker_exit_code = compilation.link(object_file_path, options.maybe_output_file_path orelse std.fs.path.stem(options.input_file_path)) catch |err| {
+            if (output_kind == .executable) {
+                const linker_exit_code = compilation.link(
+                    object_file_path,
+                    maybe_output_file_path orelse std.fs.path.stem(input_file_path),
+                ) catch |err| {
                     std.debug.print("Error: could not link object file: {s}\n", .{errorDescription(err)});
 
                     return 1;
@@ -336,6 +446,74 @@ pub const Cli = struct {
         }
 
         return 0;
+    }
+
+    fn executeCompileCommand(self: Cli) u8 {
+        const options = self.command.?.compile;
+
+        return self.compileStep(
+            options.input_file_path,
+            options.maybe_output_file_path,
+            options.output_kind,
+            options.runner_kind,
+            options.target,
+        );
+    }
+
+    fn executeRunCommand(self: Cli) u8 {
+        const options = self.command.?.run;
+
+        const output_file_path = std.fs.path.stem(options.input_file_path);
+
+        const compile_step_exit_code = self.compileStep(
+            options.input_file_path,
+            output_file_path,
+            .executable,
+            options.runner_kind,
+            options.target,
+        );
+
+        if (compile_step_exit_code != 0) return compile_step_exit_code;
+
+        const real_output_file_path = std.fs.realpathAlloc(self.allocator, output_file_path) catch |err| {
+            std.debug.print("Error: could not resolve output file path: {s}\n", .{errorDescription(err)});
+
+            return 1;
+        };
+
+        defer self.allocator.free(real_output_file_path);
+
+        const exe_process_argv = std.mem.concat(self.allocator, []const u8, &.{
+            &.{real_output_file_path},
+            options.arguments,
+        }) catch |err| {
+            std.debug.print("Error: {s}\n", .{errorDescription(err)});
+
+            return 1;
+        };
+
+        defer self.allocator.free(exe_process_argv);
+
+        const exe_process = std.process.Child.run(.{ .allocator = self.allocator, .argv = exe_process_argv }) catch |err| {
+            std.debug.print("Error: could not run executable: {s}\n", .{errorDescription(err)});
+
+            return 1;
+        };
+
+        std.debug.print("{s}", .{exe_process.stdout});
+        std.debug.print("{s}", .{exe_process.stderr});
+
+        std.fs.cwd().deleteFile(output_file_path) catch |err| {
+            std.debug.print("Error: could not delete output file: {s}\n", .{errorDescription(err)});
+
+            return 1;
+        };
+
+        switch (exe_process.term) {
+            .Exited => |code| return code,
+
+            else => return 1,
+        }
     }
 
     fn executeHelpCommand(self: Cli) u8 {
@@ -362,6 +540,7 @@ pub fn main() u8 {
 
     switch (cli.command.?) {
         .compile => return cli.executeCompileCommand(),
+        .run => return cli.executeRunCommand(),
         .help => return cli.executeHelpCommand(),
     }
 }
